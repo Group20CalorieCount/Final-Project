d590cc8bca82613d51c270a4c960124aa37d5c13
elijah548 <144622410+elijah548@users.noreply.github.com>
2023-12-03 15:49:16 -0500

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -5,6 +5,12 @@ let  path = require('path');
 let  cookieParser = require('cookie-parser');
 let  logger = require('morgan');
 
+let  app = express();
+let session = require('express-session');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy; 
+let flash = require('connect-flash');
+
 // config mongDB
 let mongoose = require('mongoose');
 let DB = require('./db');
@@ -17,21 +23,20 @@ mongDB.once('open', ()=> {
   console.log('connected to the MongoDB');
 });
 
+
 let  indexRouter = require('../../server/routes/index');
 let  usersRouter = require('../../server/routes/users');
 let  workoutRouter = require('../../server/routes/work');
 
-let  app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
@@ -42,16 +47,3 @@ app.use('/Workout-list', workoutRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
